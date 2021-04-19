1e35a96818d14e8ab03bc306047c96409b6e8661
xt <xt@163.com>
2021-04-19 13:13:26 +0800

    EmployeeController.java

--- src/main/java/cn/albert/crud/controller/EmployeeController.java
+++ src/main/java/cn/albert/crud/controller/EmployeeController.java
@@ -8,12 +8,17 @@ import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 处理员工CRUD请求
@@ -51,13 +56,28 @@ public class EmployeeController {
 
     /**
      * 员工保存
+     * 1、支持JSR303校验
+     * 2、导入Hibernate-Validator
      * @return
      */
     @RequestMapping(value = "/emp", method = RequestMethod.POST)
     @ResponseBody
-    public Msg saveEmp(Employee employee){
-        employeeService.saveEmp(employee);
-        return Msg.success();
+    public Msg saveEmp(@Valid Employee employee, BindingResult result){
+        if (result.hasErrors()){
+            //校验失败，不应该保存员工，应该返回失败，在模态框中显示校验失败的错误信息
+            Map<String, Object> map = new HashMap<>();
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                System.out.println("错误的字段名："+fieldError.getField());
+                System.out.println("错误信息："+fieldError.getDefaultMessage());
+                map.put(fieldError.getField(),fieldError.getDefaultMessage());
+            }
+            return Msg.fail().add("errorFields", map);
+
+        }else {
+            employeeService.saveEmp(employee);
+            return Msg.success();
+        }
     }
 
     /**
