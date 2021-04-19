f345a6f4d52334a274ce3bbba16e46a7ee32ce27
xt <xt@163.com>
2021-04-19 13:02:07 +0800

    Employee.java

--- src/main/java/cn/albert/crud/bean/Employee.java
+++ src/main/java/cn/albert/crud/bean/Employee.java
@@ -1,12 +1,20 @@
 package cn.albert.crud.bean;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.Pattern;
+
 public class Employee {
     private Integer empId;
 
+    @Pattern(regexp = "(^[a-z0-9_-]{6,16}$)|(^[\\u2E80-\\u9FFF]{2,5})"
+            ,message = "用户名必须是2-5位中文或者6-16位数字和字母的组合")
     private String empName;
 
     private String gender;
 
+    @Pattern(regexp = "^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$"
+            ,message = "邮箱格式不正确")
     private String email;
 
     private Integer dId;
